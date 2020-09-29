01d36ca3fe79c63b608722bae4c2270c9bbb6934
adrian <giddychutoy@gmail.com>
2020-09-29 11:03:00 +0200

    [ADD] .gitignore | clase vacía

---
+++ src/com/adri/presentacion/Vista.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.adri.presentacion;
+
+/**
+ * @author Giddy
+ *
+ */
+public class Vista {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
